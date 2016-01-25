af4fda5e3fd8be5bc6b8f95bedcd54301b426f6b
unknown <muthiang@yahoo.com>
2016-01-25 07:11:53 -0800

    concurrency bug fix, concurrency set to 20

--- process.go
+++ process.go
@@ -1,19 +1,24 @@
 package main
 import "sync"
 
+var concurrency int = 20
+
 func process(wm wordMap, wordCount int, out chan<- string) {
-	wchan := make(chan string, 20)
+	wchan := make(chan string, concurrency)
 	var wgrp sync.WaitGroup
 	wgrp.Add(wordCount)
 	go wm.getWordForProcessing(wchan)
-	go func() {
-		for i:=0; i<wordCount; i++ {
-			select {
-				case w := <-wchan:
-					go wm.processWord(w, out, &wgrp)
+	for i:=0; i<concurrency; i++ {
+		go func() {
+			for {
+				w, ok := <-wchan
+				if !ok {
+					return
+				}
+				wm.processWord(w, out, &wgrp)
 			}
-		}
-	}()
+		}()
+	}
 	wgrp.Wait()
 	close(out)
 }
